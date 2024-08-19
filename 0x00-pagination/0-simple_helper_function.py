2324cedf41cbcedd702933a66d8ba06f89de3a4e
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-19 20:29:38 +0100

    task 0 done

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Simple helper function"""
+
+
+def index_range(page, page_size):
+    """
+    function named index_range
+    that takes two integer arguments page and page_size
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)

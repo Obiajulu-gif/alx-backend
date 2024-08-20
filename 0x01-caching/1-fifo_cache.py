e1100044a88b491a7cab36ef5b065fff728f9ad4
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-20 14:52:42 +0100

    task 1 done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,6 +2,7 @@
 """1. FIFO caching"""
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
@@ -14,7 +15,7 @@ class FIFOCache(BaseCaching):
         Initialize
         """
         super().__init__()
-        self.key_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
@@ -23,14 +24,13 @@ class FIFOCache(BaseCaching):
         higher than that of BaseCaching.MAX_ITEMS
         """
         if key is not None or item is not None:
-            if key not in self.cache_data:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    first_key = self.key_order.pop(0)
-                    del self.cache_data[first_key]
-                    print("DISCARD: {}".format(first_key))
+            if key in self.cache_data:
+                del self.cache_data[key]
             self.cache_data[key] = item
-            if key not in self.key_order:
-                self.key_order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """

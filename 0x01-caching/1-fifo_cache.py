4acd2a8a3349be3c56eb2d07e12a61ece843e2e5
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-20 15:34:24 +0100

    task 1 done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -27,12 +27,16 @@ class FIFOCache(BaseCaching):
         check if the number of items in self.cache_data
         higher than that of BaseCaching.MAX_ITEMS
         """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
+        if key is not None or item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+
+        return
 
     def get(self, key):
         """

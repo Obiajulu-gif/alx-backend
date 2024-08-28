826bbbd61548a726e08a28c869a4f7978f89c148
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-28 06:24:50 +0100

    task 3 done

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,26 +6,29 @@ with parametrized locale selection
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
-    """ 
+    """
     Config class for app
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
-    
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> str:
-    """ 
+    """
     Get locale from request
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index() -> str:

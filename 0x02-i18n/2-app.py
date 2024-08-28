0a824d66164d99044d1dc390acadae4beb5068bc
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-28 02:44:40 +0100

    task 2 done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """
-A Flask app enhanced with Flask-Babel for i18n, with locale selection based on request
+A Flask app enhanced with Flask-Babel for i18n,
+with locale selection based on request
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
+
 
 class Config:
     """
@@ -13,9 +15,11 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -24,12 +28,14 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """
     Render the index.html template
     """
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

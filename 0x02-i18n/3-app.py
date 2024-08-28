ca03121d7d9095b8fb79221e017a426e2f2fbcd3
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-28 04:45:11 +0100

    task 3 done

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A Flask app enhanced with Flask-Babel for i18n,
+with parametrized locale selection
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel()
+
+babel.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('3-index.html', home_title=_("Welcome to Holberton"), home_header=_("Hello world!"))
+
+if __name__ == '__main__':
+    app.run(debug=True)

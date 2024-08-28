f4eb27b94d44ef0ea90f2dd341e518c6d8f9cfd2
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-28 06:27:22 +0100

    task 3 done

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """
-    Get locale from request
+    Get locale from request 
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 

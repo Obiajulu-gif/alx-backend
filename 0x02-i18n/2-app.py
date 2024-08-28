1eeb93c57f4b198c653c4e099ec896d00ddc13a6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-28 02:42:28 +0100

    task 2 done

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+A Flask app enchanced with Flask-babel for i18n,
+with locale selection based on request
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select a language translation for the user
+    based on the request
+    """
+    return request.accept_language.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Render the index.html template
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
